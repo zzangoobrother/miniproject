672d2bc7e33a20953bad386b5bc2ddc73bb7883b
zzangoobrother <com1368@naver.com>
2021-10-12 10:12:21 +0900

    공통 exception 구현

---
+++ src/main/java/com/miniproject/spring/exception/HanghaeMiniException.java
@@ -0,0 +1,8 @@
+package com.miniproject.spring.exception;
+
+public class HanghaeMiniException extends Exception {
+
+    public HanghaeMiniException(String message) {
+        super(message);
+    }
+}
