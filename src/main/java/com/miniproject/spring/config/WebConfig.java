6b04ff27489beec2c2064d2282a173c9baa0207a
zzangoobrother <com1368@naver.com>
2021-10-13 15:27:46 +0900

    Xss 기능 추가

--- src/main/java/com/miniproject/spring/config/WebConfig.java
+++ src/main/java/com/miniproject/spring/config/WebConfig.java
@@ -1,15 +1,22 @@
 package com.miniproject.spring.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.apache.tomcat.util.http.Rfc6265CookieProcessor;
 import org.apache.tomcat.util.http.SameSiteCookies;
 import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final ObjectMapper objectMapper;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
@@ -25,5 +32,13 @@ public class WebConfig implements WebMvcConfigurer {
             cookieProcessor.setSameSiteCookies(SameSiteCookies.NONE.getValue());
             context.setCookieProcessor(cookieProcessor);
         };
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter jsonEscapeConverter() {
+        System.out.println("filter");
+        ObjectMapper copy = objectMapper.copy();
+        copy.getFactory().setCharacterEscapes(new HtmlCharacterEscapes());
+        return new MappingJackson2HttpMessageConverter(copy);
     }
 }
