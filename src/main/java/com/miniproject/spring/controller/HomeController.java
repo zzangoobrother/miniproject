1bdaf95040c0a0b4ae7749cc9c9ba0ed080147cb
zzangoobrother <com1368@naver.com>
2021-10-14 20:50:12 +0900

    git merge 수정

--- src/main/java/com/miniproject/spring/controller/HomeController.java
+++ src/main/java/com/miniproject/spring/controller/HomeController.java
@@ -4,6 +4,10 @@ import com.miniproject.spring.model.Comment;
 import com.miniproject.spring.model.Post;
 import com.miniproject.spring.service.CommentService;
 import com.miniproject.spring.service.PostService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,8 +32,9 @@ public class HomeController {
 
     // 메인페이지 게시글 전체 조회
     @GetMapping("/posts")
-    public Map<String, Object> home() {
-        List<Post> posts = postService.home();
+    public Map<String, Object> home(@PageableDefault(page = 0,
+            size = 10, sort = "modifiedDt", direction = Sort.Direction.DESC) Pageable pageable) {
+        Page<Post> posts = postService.home(pageable);
         Map<String, Object> result = new HashMap<>();
         result.put("posts", posts);
 
